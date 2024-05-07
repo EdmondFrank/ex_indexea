5667aa5a509d38ae1bfe416df104eb3a2e0f2499
EdmondFrank <EdmondFrank@hotmail.com>
2024-05-07 15:05:28 +0800

    Support delete by query.
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- lib/ex_indexea/records.ex
+++ lib/ex_indexea/records.ex
@@ -2,8 +2,13 @@ defmodule ExIndexea.Records do
   import ExIndexea
   alias ExIndexea.Client
 
-  @spec list(Client.t(), binary(), pos_integer(), map(), Keyword.t) :: ExIndexea.response()
+  @spec list(Client.t(), binary(), pos_integer(), map(), any) :: ExIndexea.response()
   def list(client, app, index, params \\ %{}, opts \\ %{}) do
     post("/records/#{app}/#{index}", client, params, opts)
+  end
+
+  @spec delete_by_query(Client.t(), binary(), pos_integer(), map(), any) :: ExIndexea.response()
+  def delete_by_query(client, app, index, query, params \\ %{}, opts \\ %{}) do
+    delete("/records/#{app}/#{index}/bulk", client, Map.merge(params, %{query: query}), opts)
   end
 end
